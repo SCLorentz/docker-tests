21093f7b1e6cd77d9cc2c0f7884dfb2b813764cb
Felipe S.C. Lorentz <113468900+SCLorentz@users.noreply.github.com>
2025-03-13 17:05:52 +0000

    help

--- becca/.devcontainer/cmd/.bash
+++ becca/.devcontainer/cmd/.bash
@@ -14,3 +14,15 @@ refresh()
     IFS= read -r -n1 -s key
     printf "\x1b[38;5;245m\x1b[3mnow you entered in the terminal, good luck!\x1b[0m\n\x1B[?25h"
 }
+
+help()
+{
+    if [[ "$1" == "git" ]]; then
+        printf "use git push to synchronize your project with the web\nuse git pull to get the last version of your project\nuse git checkout to go back and forward to different versions of your program\n"
+    elif [[ "$1" == "deno" ]]; then
+        printf "use deno run /path/to/your/file.ts to run your JS/TS program\n"
+    else
+        printf "Usage: help <command>\nAvailable commands: git, deno\n"
+    fi
+    printf "\n"
+}
